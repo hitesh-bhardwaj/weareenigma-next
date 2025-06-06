dc31544addd7caf38f32c076f483a77358b42504
Hitesh Bhardwaj <bhardwajhitesh09@gmail.com>
2025-06-06 12:00:01 +0530

    Update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <GlobalCanvas
-        style={{ pointerEvents: 'none', height: '100vh', width: '100vw', position: 'fixed', top: 0, left: 0 }}
+        style={{ pointerEvents: 'none', height: '100vh', width: '100vw', position: 'fixed', top: 0, left: 0, zIndex: 1 }}
         gl={{ antialias: true }}
       >
         <ambientLight />
--- src/components/homepage/Footer/index.jsx
+++ src/components/homepage/Footer/index.jsx
@@ -1,7 +1,6 @@
 import { Suspense } from "react";
 import FooterCopy from "./FooterCopy";
 import { FractalGlassModelWrapper } from "@/components/TestingComponents/FractalGlassModel";
-import FooterCTA from "./FooterCTA";
 import FooterCTACopy from "./FooterCTACopy";
 import { EnigmaModel } from "./EnigmaModel";
 
@@ -9,23 +8,25 @@ export const Footer = (() => {
 
     return (
         <>
-            <section className='h-full w-full  overflow-hidden relative' id='hero-section'>
+            <section className='h-full w-full overflow-hidden relative' id='hero-section'>
                 <div className="flex h-[50vw] bg-black-1">
                     <div className="w-[60%]">
-                    <FooterCTACopy />
+                        <FooterCTACopy />
                     </div>
-                    <div className="w-[40%] h-full">
+                    <div className="w-[40%] h-full relative">
                         <Suspense>
                             <EnigmaModel />
                         </Suspense>
                     </div>
                 </div>
 
-                <FooterCopy />
-                <div className="absolute bottom-0 left-0 h-screen">
-                    <Suspense>
-                        <FractalGlassModelWrapper img={"/assets/models/hero-bg.png"} />
-                    </Suspense>
+                <div className="h-full w-full relative">
+                    <FooterCopy />
+                    <div className="absolute top-[20%] left-0 h-screen w-screen">
+                        <Suspense>
+                            <FractalGlassModelWrapper img={"/assets/models/hero-bg.png"} />
+                        </Suspense>
+                    </div>
                 </div>
 
             </section>
